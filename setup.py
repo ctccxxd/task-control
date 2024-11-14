d3c50e1da1095bb0e10b1f4a8becda6244e3d702
Shane <imu_xxd@163.com>
2024-11-14 14:28:43 +0800

    revise setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="framework for flow-control",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ctccxxd/task-dag-control",
+    url="https://github.com/ctccxxd/task-control",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
