cd9b989d5b2bffb002feff775f887a7b968d4dae
Shane <imu_xxd@163.com>
2024-11-15 17:19:40 +0800

    revise format

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="taskmanager",
-    version="0.0.2",
+    version="0.0.1",
     author="Shane",
     author_email="imu_xxd@vip.qq.com",
     description="framework for flow-control",
--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,4 +2,3 @@ from .lib import TaskPool, StepResultCode, step_retry_delay
 
 name = "taskmanager"
 __all__ = [TaskPool, StepResultCode, step_retry_delay]
-
