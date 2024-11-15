d1a94a5e5765ce3d5e438608fe4ee0abf58a9574
Shane <imu_xxd@163.com>
2024-11-15 14:46:20 +0800

    revise format

--- taskmanager/__int__.py
+++ taskmanager/__int__.py
@@ -1,4 +1,4 @@
 from .lib import TaskPool, StepResultCode, step_retry_delay
 
 name = "taskmanager"
-__all__ = [TaskPool, StepResultCode, step_retry_delay]
+__all__ = ["TaskPool", "StepResultCode", "step_retry_delay"]
